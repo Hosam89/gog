c7da6f7f0706dc974420db94a8f5852dbb4771b7
Silte1 <etiennesilue3@gmail.com>
2022-11-12 20:57:26 +0100

    The second commit

---
+++ main1.js
@@ -0,0 +1,18 @@
+let countDownDate = new Date("Nov 14, 2022 15:37:25").getTime();
+let x = setInterval(function() {
+  let now = new Date().getTime();
+  let distance = countDownDate - now;
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  for (let i = 0; i < 5; i++) {
+    document.getElementsByClassName("demo")[i].innerHTML = `${days} d: ${hours} h ${minutes} min:  ${seconds} sec
+    Verbleibend`;
+    if (distance < 0) {
+      clearInterval(x);
+      document.getElementsByClassName("demo")[i].innerHTML = "EXPIRED";
+    }
+    
+  }
+}, 1000);
